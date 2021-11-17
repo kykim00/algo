5a0a867e6b1134faa0ad6b55b8c1ab9df98cfea0
kykim00 <93499158+kykim00@users.noreply.github.com>
2021-11-17 18:35:29 +0900

    크레인 인형뽑기 게임

--- 1week/03_pickup.py
+++ 1week/03_pickup.py
@@ -1,16 +1,16 @@
-board=[[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
-moves=[1,5,3,5,1,2,1,4]
-new_board = list(map(list, zip(*board)))
-stack= []
-answer= 0
-for i in moves:
-    for j in new_board[i-1]:
-        if j > 0:
-            stack.append(j)
-            new_board[i-1].remove(j)
-            break
-    if len(stack) >= 2 and stack[-1] == stack[-2]:
-        stack.pop()
-        stack.pop()
-        answer += 2
-print(answer)
+def solution(board, moves):
+    # board의 행 열 전환
+    new_board = list(map(list, zip(*board)))
+    stack = []
+    answer = 0
+    for i in moves:
+        for j in new_board[i-1]:
+            if j > 0:
+                stack.append(j)
+                new_board[i-1].remove(j)
+                break
+        if len(stack) >= 2 and stack[-1] == stack[-2]:
+            stack.pop()
+            stack.pop()
+            answer += 2
+    return answer
