7a36d645cc3b0178db31404d338b40d38d746511
kykim00 <93499158+kykim00@users.noreply.github.com>
2021-12-08 18:21:06 +0900

    Day 5

---
+++ JS100제/Day5/013.js
@@ -0,0 +1,3 @@
+let planet = ['수성', '금성', '지구', '화성', '목성', '토성', '천왕성', '해왕성'];
+let n = prompt();
+console.log(planet[n-1]);
---
+++ JS100제/Day5/015.js
@@ -0,0 +1,2 @@
+let name = prompt();
+console.log(`안녕하세요. 저는 ${name}입니다`);
