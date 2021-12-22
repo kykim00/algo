d925f21e00827eb50600ac3381648104bded8006
kykim00 <93499158+kykim00@users.noreply.github.com>
2021-12-22 22:32:54 +0900

    Delete JS100제/Day6 directory

--- JS100제/Day6/017.js
+++
@@ -1,6 +0,0 @@
-let height = prompt();
-if (height >= 150) {
-    console.log('YES');
-} else {
-    console.log('NO');
-}
--- JS100제/Day6/018.js
+++
@@ -1,6 +0,0 @@
-let [a, b, c] = prompt().split(' ');
-// a, b, c를 반복문 없이 한번에 바꿀 수 없을까
-a = +a;
-b = +b;
-c = +c;
-console.log((a+b+c)/3);
